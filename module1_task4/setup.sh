d984bf086f4febb91407fe7500c9dcece77786b3
Your Name <michaelgolenia1@gmail.com>
2023-01-08 19:09:36 -0300

    Nueva forma

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-apt-get update && apt-get install -y git make curl build-essentiali
+sudo apt-get update && sudo apt-get install -y make wget build-essential
+wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
+dpkg -i hugo_0.79.0_Linux-64bit.deb
+make build
+exit 0
